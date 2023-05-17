1987d5584efac569bda64aae4bc7493c2408426d
Sergey <Zaikouski.sergey@gmail.com>
2023-05-17 11:42:58 +0300

    08 homework

--- src/s2-homeworks/hw08/bll/homeWorkReducer.ts
+++ src/s2-homeworks/hw08/bll/homeWorkReducer.ts
@@ -4,15 +4,24 @@ type ActionType =
     | { type: 'sort'; payload: 'up' | 'down' }
     | { type: 'check'; payload: number }
 
-export const homeWorkReducer = (state: any, action: any): any => { // need to fix any
+export const homeWorkReducer = (state: UserType[], action: ActionType): UserType[] => { // need to fix any
     switch (action.type) {
         case 'sort': { // by name
-
-            return state // need to fix
+            switch (action.payload) {
+                case "up" : {
+                    return [...state].sort((a, b) =>
+                        a.name.localeCompare(b.name))
+                }
+                case "down": {
+                    return [...state].sort((a, b) =>
+                        b.name.localeCompare(a.name))
+                }
+                default:
+                    throw new Error('I don\'t understand this type')
+            }
         }
         case 'check': {
-
-            return state // need to fix
+            return state.filter(a => a.age > 18)
         }
         default:
             return state
