9aaedb7bd74bbf09c41ab17e9b8eef0e10ee1228
Sergey <Zaikouski.sergey@gmail.com>
2023-07-04 13:33:15 +0300

    fix loading

--- src/s2-homeworks/hw14/HW14.tsx
+++ src/s2-homeworks/hw14/HW14.tsx
@@ -35,7 +35,7 @@ const HW14 = () => {
         getTechs(value)
             .then((res) => {
                 // делает студент
-                //setLoading(false)
+                setLoading(false)
                 if (res) {
                     setTechs(res.data.techs)
                 }
